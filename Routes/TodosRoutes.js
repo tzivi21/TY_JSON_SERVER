b27ecc1c7b6587a7ddda6bb6bf3af9d3fa13c69f
YehuditSa <yehudit2003@gmail.com>
2024-04-02 00:42:59 +0300

    huhuhu

--- Routes/TodosRoutes.js
+++ Routes/TodosRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const TodosController = require("../Controllers//TodosController");
+const TodosController = require("../Controllers/TodosController");
 
 router.get('/', TodosController.getAllTodos);
 
